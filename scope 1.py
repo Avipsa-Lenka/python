4378eb9d17d9ee96317b49f895ecc82b3b93031c
Avipsa-Lenka <75486924+Avipsa-Lenka@users.noreply.github.com>
2020-12-04 20:02:53 +0530

    details : basic code

---
+++ scope 1.py
@@ -0,0 +1,20 @@
+print("hello world")
+print('this "movie" is awesome')
+print("tina")
+print("hello","world",sep=",")
+print("hello","world",sep="\n\n")
+print("hello","world",end=".",sep="\n")
+a=10
+b=20
+print("the sum of {0} and {1} is {2}".format(a,b,a+b))
+print(f"the sum of {a} and {b} is {a+b}")
+name="avipsa"
+location="odisha"
+print(f"i am {name} from {location}")
+a=int(input())
+b=int(input())
+print(f"the sum of {a} and {b} is {a+b}")
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+print(f"the product of {a} and {b} and {c} is {a*b*c}")
